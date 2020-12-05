84bcc4e6619d2d8071173e96ede33e68b3103c40
Mina Daoud <minadaoud1983@gmail.com>
2020-12-05 18:06:40 +0100

    Storing short URL in Redis

--- src/main/java/com/boa/test/UrlShortner/config/RedisConfig.java
+++ src/main/java/com/boa/test/UrlShortner/config/RedisConfig.java
@@ -1,7 +1,10 @@
 package com.boa.test.UrlShortner.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import redis.embedded.RedisServer;
 
 import javax.annotation.PostConstruct;
@@ -11,16 +14,31 @@ import java.io.IOException;
 @Configuration
 public class RedisConfig {
     private RedisServer redisServer;
-    @Value("${spring.redis.port}") int redisPort;
+    @Value("${spring.redis.host}")
+    String redisHost;
+    @Value("${spring.redis.port}")
+    int redisPort;
 
     @PostConstruct
     public void startRedis() throws IOException {
-        redisServer  = new RedisServer(redisPort);
+        redisServer = new RedisServer(redisPort);
         redisServer.start();
     }
 
     @PreDestroy
-    public void stopRedis(){
+    public void stopRedis() {
         redisServer.stop();
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
     }
 }
--- src/main/java/com/boa/test/UrlShortner/service/BasicUrlShortnerService.java
+++ src/main/java/com/boa/test/UrlShortner/service/BasicUrlShortnerService.java
@@ -1,16 +1,24 @@
 package com.boa.test.UrlShortner.service;
 
 import com.google.common.hash.Hashing;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 
 @Service
 public class BasicUrlShortnerService implements UrlShortnerService {
+    RedisTemplate redisTemplate;
+
+    public BasicUrlShortnerService(RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     @Override
     public String shortenUrl(String longUrl, String domainUrl) {
         final String shortUrlId = Hashing.murmur3_32().hashString(longUrl, StandardCharsets.UTF_8).toString();
-
-        return domainUrl + shortUrlId;
+        redisTemplate.opsForValue().set(shortUrlId, longUrl);
+        redisTemplate.opsForValue().get(shortUrlId);
+        return domainUrl + "/" + shortUrlId;
     }
 }
